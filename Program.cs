d2caf733d4cb2510c4203f7db139757e54f2af2b
Anoreloth <93758322+Anoreloth@users.noreply.github.com>
2021-12-16 16:46:16 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB5_2
+{
+    interface IPrintable
+    {
+        void print();
+    }
+    class Book : IPrintable
+    {
+        public void print()
+        {
+            Console.WriteLine("Book");
+        }
+    }
+    class Magazine : IPrintable
+    {
+        public void print()
+        {
+            Console.WriteLine("Magazine");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPrintable[] pr = new IPrintable[6];
+            pr[0] = new Book();
+            pr[1] = new Magazine();
+            pr[2] = new Book();
+            pr[3] = new Magazine();
+            pr[4] = new Book();
+            pr[5] = new Magazine();
+            foreach (var printable in pr)
+            {
+                printable.print();
+            }
+        }
+    }
+
+}
